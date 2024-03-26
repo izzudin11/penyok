518371669955f5989e1a966bad1d17de49c4c8be
izzudin11 <rjr40963@gmail.com>
2024-03-26 11:22:13 +0700

    pertama

---
+++ src/data/piupiu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author acer
+ */
+public class piupiu {
+   /**
+    * creates new form piupiu
+    * */
+    double jumlah,bil1,bil2;
+    int pilih;
+    String bilangan;
+    
+    public kalkulator(){
+        initComponents();
+        bilangan="";
+    }
+    
+}
